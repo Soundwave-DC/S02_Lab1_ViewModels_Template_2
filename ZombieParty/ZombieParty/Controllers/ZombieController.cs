e4ec0244e80ce78568541b31ec47328444a46530
Aurélien McDougall <2265255@cegepmontpetit.ca>
2026-09-01 15:06:31 -0400

    FCT Zombie Index et Create Et Validation avec messages d'erreur
    Maintenant possible de voir la liste de zombies et d'en ajouter avec gestion d'erreurs

--- ZombieParty/ZombieParty/Controllers/ZombieController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ZombieParty.Models;
 
 namespace ZombieParty.Controllers
@@ -8,8 +9,45 @@ namespace ZombieParty.Controllers
         public IActionResult Index()
         {
             this.ViewBag.MaListe = new List<Zombie>();
+            this.ViewBag.MaListe = _baseDonnees.Zombies.ToList();
 
             return View();
         }
+
+        public IActionResult Create()
+        {
+            ViewBag.ZombieTypes = new SelectList(_baseDonnees.ZombieTypes.ToList(), "Id", "TypeName", null);
+            return View();
+        }
+
+
+        private BaseDonnees _baseDonnees { get; set; }
+
+        public ZombieController(BaseDonnees baseDonnees)
+        {
+            _baseDonnees = baseDonnees;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Zombie zombie)
+        {
+            //Si le modèle est valide le zombie est ajouté et nous sommes redirigés vers index.
+            if (ModelState.IsValid)
+            {
+                _baseDonnees.Zombies.Add(zombie);
+                TempData["Success"] = $"Zombie {zombie.Name} added";
+                return this.RedirectToAction("Index");
+            }
+            //Il faut repopuler le zombieType dans le ViewBag
+            //Allez chercher le ZombieType sélectionné, rappel 2W5 Linq
+            ZombieType selectedZombieType = _baseDonnees.ZombieTypes.Where(zt => zt.Id == zombie.ZombieTypeId).SingleOrDefault();
+            zombie.ZombieType = selectedZombieType;
+
+            ViewBag.ZombieTypes = new SelectList(_baseDonnees.ZombieTypes.ToList(), "Id", "TypeName", selectedZombieType);
+
+            return View(zombie);
+        }
+
+
     }
 }
